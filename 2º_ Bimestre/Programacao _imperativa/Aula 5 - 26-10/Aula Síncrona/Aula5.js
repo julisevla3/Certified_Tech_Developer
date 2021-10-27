f24f108eb51ed86c23036a618ad51bbd50f3a049
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-10-27 07:31:03 -0300

    update aula 5

---
+++ 2º_ Bimestre/Programacao _imperativa/Aula 5 - 26-10/Aula Síncrona/Aula5.js
@@ -0,0 +1,20 @@
+
+function pessoa (nome, idade) {
+    return nome + " tem" + idade;
+
+}
+
+
+console.log( pessoa("Juliane",31));
+
+function somar(x, y) {
+    return x + y;
+}
+let result = somar(15,5);
+console.log(somar(20,15));
+console.log(somar(8,15));
+console.log(somar(90,15));
+
+console.log("Esse é o resultado: " + result);
+
+console.log(Math.random ());
---
+++ 2º_ Bimestre/Programacao _imperativa/Aula 5 - 26-10/Mesa_de_Trabalho/Aula5.js
@@ -0,0 +1,57 @@
+
+// O que cada função retorna?
+// Neste exercício, você terá que pensar sobre o que cada função retornará, sem executá-la no console.
+
+function teste1 (x,y) {
+    return 
+}
+
+
+console.log(teste1(10,40));
+
+//Retorna 30
+
+function teste2 (x,y) {
+    return x * 2
+    console.log(x)
+    return x / 2
+}
+
+console.log(teste2(10))
+
+
+
+// Funções Simples
+// Crie uma função que converta polegadas em centímetros. 
+// Recebe pelo parâmetro polegadas e retorna seu equivalente em centímetros.
+// Crie uma função que receba uma string e a converta em um URL.
+// ex: "funcaojs" é retornado como "http://www.funcaojs.com.br"
+// Crie uma função que recebe uma string e retorna a mesma frase, mas com o caracter de exclamação ( ! ).
+// Crie uma função que calcule a idade dos cachorros, considerando que 1 ano humano, equivale a 7 anos para eles.
+// Crie uma função que calcule o valor da sua hora de trabalho, tendo como parâmetro o seu salário mensal.
+// PS: considere que você trabalhe 160 horas no mês.
+// Crie a função calculadora de IMC* de uma pessoa, que recebe a altura em centímetros e o peso em quilogramas. Em seguida, execute a função, testando diferentes valores. 
+// *IMC, significa Índice de Massa Corporal e é um parâmetro utilizado para saber se o peso está de acordo com a altura.
+
+// Se chegarmos a este ponto, ficaremos muito satisfeitos. 
+// Agora, deixamos alguns exercícios com uma dificuldade extra: você terá que buscar alguns conceitos extras para poder resolver estes exercícios. É uma prática que os programadores fazem todos os dias. Como sempre dizemos, uma parte importante do aprendizado em programação, é ignorar a complexidade e ir estritamente ao que precisamos, sabemos que não é uma prática fácil de fazer, mas com o tempo se tornará mais fácil e divertida.
+// Crie uma função que receba uma string em minúsculas, converta-a em maiúsculas e as retorne. 
+// Investigue o que o método de .toUpperCase() faz.
+// Crie uma função que receba um parâmetro e retorne o tipo de dado deste parâmetro.
+// Dica: Isso te ajudará a entender o que o operador typeof faz.
+// Crie uma função pela qual passamos o raio de um círculo e ela retorna a circunferência.
+// Dica: Investigue se o objeto Math tem entre suas propriedades o número Pi.
+
+function polegada(num){
+    const conv = 2.54
+    return num * conv
+}
+console.log(polegada(10));
+
+function convString(texto){
+       return "http://www." + texto + ".com.br"
+}
+console.log(convString("azul"));
+
+
+function strign
