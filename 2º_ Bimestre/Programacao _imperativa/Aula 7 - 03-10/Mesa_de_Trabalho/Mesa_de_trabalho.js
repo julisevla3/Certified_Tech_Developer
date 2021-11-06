60fadcfbfb906ce12eb4a71838e6e3beb376e2e0
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-11-06 18:56:37 -0300

    Update Mesa_de_trabalho.js

--- 2º_ Bimestre/Programacao _imperativa/Aula 7 - 03-10/Mesa_de_Trabalho/Mesa_de_trabalho.js
+++ 2º_ Bimestre/Programacao _imperativa/Aula 7 - 03-10/Mesa_de_Trabalho/Mesa_de_trabalho.js
@@ -1,39 +1,40 @@
 // Ex1. O que cada expressão retorna?
 // Dê uma olhada nestes exemplos e pense sobre o que cada um retorna:
-!true
-!false
-!!false
-!!true
-!1
-!!1
-!0
-!!0
-!!"" 
+console.log (!true);
+console.log (!false);
+console.log (!!false);
+console.log (!!true);
+console.log (!1);
+console.log (!!1);
+console.log (!0);
+console.log (!!0);
+console.log (!!"" );
+
 let x = 5 ;
 let y = 9;
-x < 10 && x!==5
-x>9 || x===5
-!(x===y)
+console.log (x < 10 && x!==5);
+console.log (x>9 || x===5);
+console.log (!(x===y));
 
 
 // Ex2. O que cada expressão retorna?
 // Sem testar no console, o que você acha que cada uma destas expressões irá retornar? Elas são true ou false?
 
-let x=10
-let y ="a"
-y==="b" || x >= 10
+let x=10;
+let y ="a";
+console.log (y==="b" || x >= 10);
 
 //  True
 
-let x=3
-let y=8
-!(x == "3" || x === y) && !(y !== 8 && x <= y)
+let x=3;
+let y=8;
+console.log (!(x == "3" || x === y) && !(y !== 8 && x <= y));
 
 //True
 
-let str = ""
-let msg = "haha!"
-let eBonito = "false"
-!((str || msg) && eBonito)
+let str = "";
+let msg = "haha!";
+let eBonito = "false";
+console.log (!((str || msg) && eBonito));
 
 // False
