ec581a1ecf20d2739bfa04176c1162a29e7ef414
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-11-08 19:56:36 -0300

    update

---
+++ 2º_ Bimestre/Programacao _imperativa/Aula 8 - 08-11/exercicio/javacon.js
@@ -0,0 +1,33 @@
+// let emprego = "aprovado";
+
+// if (emprego == "aprovado") {
+//     console.log("Seja bem vinda Juliane Alves");
+// } else if (emprego == "aguardando") {
+//     console.log("Aguarde a proxima etapa");
+// } else {
+//     console.log("Infelizmente nesse momento não fou possivel seu ingresso conosco ");
+// }
+
+// If Terciario 
+// let horas = "manha";
+// let resposta = horas == "manha" ? "Hora de tomar café " : "Hora do Almoço ";
+// console.log(resposta);
+
+
+//Switch
+let horas = "";
+switch (horas) {
+
+    case "manha":
+        console.log("Hora de tomar café");
+        break;
+
+    case "tarde":
+        console.log("Hora do Almoço");
+        break;
+    case "noite":
+        console.log("Hora de Jantar");
+        break;
+    default:
+        console.log("Eu não irei comer")
+}
