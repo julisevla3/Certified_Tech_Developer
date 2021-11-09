08331d843881191e9a72a10cf3b9a91f6d4bbcb7
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-11-08 23:22:03 -0300

    Aula08

--- 2º_ Bimestre/Programacao _imperativa/Aula 8 - 08-11/Aula Síncrona/aula-8.js
+++ 2º_ Bimestre/Programacao _imperativa/Aula 8 - 08-11/Aula Síncrona/aula-8.js
@@ -1,25 +1,41 @@
 //if, else if and else
 
-let idade = 18
+// let idade = 18
 
-if (idade > 18) {
-    console.log("entrou no if");
+// if (idade > 18) {
+//     console.log("entrou no if");
 
 
-} else if (idade == 18) {
-    console.log("entrou no else if");
-} else {
-    console.log("entrou no else");
-}
-switch (idade) {
-    case 15:
-        console.log("idade é igual a 15");
-        break;
-}
-// Switch
+// } else if (idade == 18) {
+//     console.log("entrou no else if");
+// } else {
+//     console.log("entrou no else");
+// }
+// switch (idade) {
+//     case 15:
+//         console.log("idade é igual a 15");
+//         break;
+// }
+// // Switch
 
-switch (idade) {
-    case 18:
-        console.log("idade é igual a 18");
-        break;
-}
+// switch (idade) {
+//     case 18:
+//         console.log("idade é igual a 18");
+//         break;
+// }
+// let status = true;
+// if (true) {
+//     console.log("O valor é true - verdadeiro")
+
+// } else {
+//     ("O valor é false - falso")
+// }
+
+
+// let linguagem = "java";
+// if (linguagem == "javasccript") {
+//     console.log("Eu estou aprendendo")
+
+// } else {
+//     console.log("Aprenderei mais tarde");
+// }
