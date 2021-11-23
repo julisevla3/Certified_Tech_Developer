5077f1abd7a68c381f6bf6ce439f66635fe580e4
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-11-22 23:42:42 -0300

    aula 13

--- 2º_ Bimestre/Programacao _imperativa/Aula 13 - 22-11/Aula Síncrona/aula13.js
+++ 2º_ Bimestre/Programacao _imperativa/Aula 13 - 22-11/Aula Síncrona/aula13.js
@@ -1,17 +1,57 @@
-let tenista = {
-    nome: 'Juliane',
+// // let tenista = {
+// //     nome: 'Juliane',
 
-    sobrenome: 'Alves',
+// //     sobrenome: 'Alves',
 
-    saudacao: function() {return 'Olá, me chamo ' + this.nome; }
+// //     saudacao: function() {return 'Olá, me chamo ' + this.nome; }
 
-};
+// // };
 
-console.log(tenista.saudacao());
+// // console.log(tenista.saudacao());
 
-function Carro(marca, modelo){
+// // function Carro(marca, modelo){
 
-    this.marca = marca;
-    this.modelo = modelo;
+// //     this.marca = marca;
+// //     this.modelo = modelo;
     
-};
+// // };
+
+// let carro = {
+//     marca: 'ford',
+//     modelo: 'k',
+//     andar: function() {
+//         return "carro "+ this.modelo+" andando";
+
+//      }
+// }
+
+// console.log("carro andando")
+
+
+
+// function Carro(x,modelo){
+//     this.marca = x;
+//     this.modelo = modelo;
+// }
+
+// let carro = [];
+// carro.push ( new Carro("Tesla", "z"));
+// carro.push ( new Carro("Tesla", "1"));
+// carro.push ( new Carro("Tesla", "2"));
+
+// let carroNovo = new Carro("Tesla", "x");
+// let carroNovo1 = new Carro("Tesla", "3");
+
+// console.log(carroNovo);
+
+
+// JSON 
+
+let dadosCarros = {
+    "cor" :"vermelho",
+    "marca": "ford",
+    "placa":"xdc-785",
+    "qntPortas": 4
+}
+// console.log(dadosCarros);
+console.log(dadosCarros.cor);
