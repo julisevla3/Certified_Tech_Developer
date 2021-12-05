aabdcfa9d1e93c83f4f1ece5ec6948064c98b116
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2021-12-05 11:12:10 -0300

    Update Mesa16.js

--- 2º_ Bimestre/Programacao _imperativa/Aula 16/Mesa_de_Trabalho/Mesa16.js
+++ 2º_ Bimestre/Programacao _imperativa/Aula 16/Mesa_de_Trabalho/Mesa16.js
@@ -8,30 +8,52 @@
 // correspondente;
 
 // ● Se o número a ser impresso for um múltiplo do seu SEGUNDO parâmetro,
-// você deve imprimir a palavra &quot;Buzz&quot; em vez do número correspondente;
+// você deve imprimir a palavra Buzz; em vez do número correspondente;
 // ● Se o número for um múltiplo de AMBOS os parâmetros, você deve imprimir
 // a palavra “FizzBuzz”.
+
+
+
+let FizzBuzz = (numero1, numero2) => {
+    for(let i = 1; i<=100; i++) {
+        if (i % numero1 == 0 && i % numero2 == 0) {
+            console.log("FizzBuzz")
+            
+        } else if (i % numero1 == 0) {
+            console.log("Fizz")
+        } else if (i % numero2 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i);
+        }
+       
+    }
+
+}
+
+console.log(FizzBuzz(2,3));
 
 // Avançado: Como crédito extra, pense em como otimizar seu programa para que
 // as palavras sejam passadas de forma dinâmica e seja você quem decide quais
 // palavras ele deve dizer.
 
-let FizzBuzz = (a,b) => {
-    for (let i=1; i<=100; i++){
-    }
-     if((i%a==0) && (i%b==0)){
-                
-    }
-        else if (i%a==0){
-            console.log('Fizz')
+let FizzBuzz = (numero1, numero2) => {
 
-        }
-        else if (i%b==0){
-            console.log('Buzz')
+
+    for(let i = 1; i<=100; i++) {
+        let resposta;
+
+        if (i % numero1 == 0 ) {
+            resposta = "Fizz"; 
+        } 
+        
+        if (i % numero2 == 0) {
+             resposta = resposta === undefined ? "Buzz" : resposta + "Buzz";
+        } 
+
+        console.log(resposta === undefined ? i : resposta);
     }
-    else{
-        console.log(i);
-    }
+
 }
 
-FizzBuzz(2,6);
+console.log(FizzBuzz(2,3));
