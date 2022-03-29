cbfc18cc47b9d0d74096850ff8f5458595cf9651
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-03-28 23:24:40 -0300

    update file

---
+++ 3º Bimestre/Infra/Aula_28-03/aula8.py
@@ -0,0 +1,36 @@
+# nome = "Juliane"
+# print(nome)
+# nome = 10
+# print(nome) 
+
+# lista_aluno = ["Juliane", "Alves", "de lima"]
+# print(lista_aluno [2]);
+# # Adicionando item
+# lista_aluno.append("casada")
+# print(lista_aluno)
+
+# lista_aluno.remove("de lima")
+# print(lista_aluno)
+
+
+
+# dados_pessoais = {'nome': 'Juliane', 'idade':36, 'altura': 1.84}
+# print(dados_pessoais)
+
+
+
+# idade = 17
+# socio = True
+
+# if idade > 18:
+#     print("não pode entrar no bar")
+# elif idade == 18 or socio == True:
+#         print("Ano que vem voce entra")
+# else:
+#     print("Não pode entrar")
+
+
+
+for i in range(1,10):
+    print(i)
+
