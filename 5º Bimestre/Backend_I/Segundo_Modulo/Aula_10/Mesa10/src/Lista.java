aaf77b41a80bb1bd16d180a9f568802f830cf02a
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-08-12 19:42:56 -0300

    update file

---
+++ 5º Bimestre/Backend_I/Segundo_Modulo/Aula_10/Mesa10/src/Lista.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Lista {
+
+    List<Integer> listaInteiros;
+
+
+}
---
+++ 5º Bimestre/Backend_I/Segundo_Modulo/Aula_10/Mesa10/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        O comprimento da lista é maior que 5
+
+    }
+}
