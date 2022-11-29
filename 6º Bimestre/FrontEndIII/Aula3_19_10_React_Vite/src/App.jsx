880f78be2ef580759306d6f4fcfdd7564e0e1302
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-11-29 20:31:18 -0300

    update file

---
+++ 6º Bimestre/FrontEndIII/Aula3_19_10_React_Vite/src/App.jsx
@@ -0,0 +1,47 @@
+function NomeDaFuncao() {
+  const nome = "Wesley";
+
+
+return<h1 className="titulo">{nome }</h1>
+}
+
+function NomeDaFuncao2() {
+  const nome = "Wesley";
+
+
+return<h1 className="titulo">{nome }</h1>
+}
+  
+
+
+
+export {NomeDaFuncao, NomeDaFuncao2} 
+
+
+// export default NomeDaFuncao
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//     return (
+//     <div>
+//       <h1>Hello Word</h1>
+//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam libero a magni impedit aspernatur sit in sint facilis blanditiis debitis laboriosam, ex alias nesciunt? Dolorem eligendi ullam rem blanditiis ducimus!</p>
+//       </div>
+//   );
+// }
+
+// export default App
---
+++ 6º Bimestre/FrontEndIII/Aula3_19_10_React_Vite/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+  import {NomeDaFuncao,NomeDaFuncao2} from './App'
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NomeDaFuncao />
+  </React.StrictMode>
+)
